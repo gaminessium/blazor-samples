9cee2d9623f2ff92d222ecc1de9768c144668a06
長峰大河 <t_ngmn@outlook.jp>
2020-12-09 22:07:26 +0900

    Add: ライブラリを用いた画面描画処理を追加

---
+++ BlazorSamples/Client/Pages/FetchData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorSamples.Shared;
+using Microsoft.JSInterop;
+
+namespace BlazorSamples.Client.Pages
+{
+    public partial class FetchData
+    {
+        protected async void CreateGraph(IReadOnlyCollection<WeatherForecast> forecasts)
+        {
+            var temparetures = new {
+                labels = forecasts.Select(f => f.Date.ToShortDateString()),
+                datasets = new [] { 
+                    new {
+                        label = "tempC",
+                        data = forecasts.Select(f => f.TemperatureC),
+                    },
+                    new {
+                        label = "tempF",
+                        data = forecasts.Select(f => f.TemperatureF),
+                    }
+                }
+            };
+
+            var module = await JSRuntime.InvokeAsync<IJSObjectReference>(
+                "import", "./js/chart-companion.js"
+            );
+
+            await module.InvokeVoidAsync(
+                "createGraph",
+                graphCanvas,
+                temparetures
+            );
+        }
+    }
+}
---
+++ BlazorSamples/Client/wwwroot/js/chart-companion.js
@@ -0,0 +1,34 @@
+﻿class ColorPicker {
+    static #colors = ['coral', 'lightgreen', 'skyblue', 'slateblue', 'gold'];
+    static #index = 0;
+
+    static get nextColor() {
+        const idx = this.#index++ % this.#colors.length;
+        return this.#colors[idx];
+    }
+}
+
+
+export function createGraph(context, temparetures) {
+    if (context.toString() !== '[object HTMLCanvasElement]') return
+
+    const data = {
+        labels: temparetures.labels,
+        datasets: temparetures.datasets.map(d => (
+            {
+                label: d.label,
+                data: d.data,
+                tension: 0.5,
+                borderColor: ColorPicker.nextColor,
+            }
+        ))
+    };
+
+    new Chart(context, {
+        type: 'line',
+        data: data,
+        options: {
+            responsive: true,
+        }
+    });
+}
